01b693630914c089daa7a8fc131d2315e5e47cdb
Орлов Антон <fogelo@yandex.ru>
2023-03-10 11:34:40 +0300

    появление блока

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const blockObserver = new IntersectionObserver((entries) => {
-    console.log(entries);
+    console.log(entries[0])
+    if (entries[0].isIntersecting) {
+        console.log(entries[0].target.style)
+        document.querySelector(".block-3").classList.add("block-3_active")
+    } else {
+        document.querySelector(".block-3").classList.remove("block-3_active")
+    }
 }, {
-    // threshold: 1.0 
+    // threshold: 0.25
 })
 
-blockObserver.observe(document.querySelector(".block-4"))
+blockObserver.observe(document.querySelector(".block-3"))
